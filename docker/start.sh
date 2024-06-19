500e442acbfc7af633a431b8adef63d5799da7d7
Klaus Illmayer <klaus.illmayer@oeaw.ac.at>
2024-06-19 16:55:31 +0200

    Adaptions on startup script: chown to local user, have default values if environment variables not set and integrate trusted host

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# as we moved some directories in the Dockerfile, we need to set the full drupal-installation to the local user
+sudo chown -R 1000:1000 /var/www/drupal/
+
 if [[ -f /var/www/drupal/settings/settings.php ]]
 then
   echo "settings.php found, using it"
@@ -8,18 +11,23 @@ else
   # cp /var/www/drupal/html/web/sites/default/default.settings.php /var/www/drupal/html/web/sites/default/settings.php
   mkdir /var/www/drupal/settings
   cp /var/www/drupal/html/web/sites/default/default.settings.php /var/www/drupal/settings/settings.php
-  sudo sed -i /var/www/drupal/root/drupal_settings.php \
-    -e "s/DBNAME/$DBNAME/g" \
-    -e "s/DBUSER/$DBUSER/g" \
-    -e "s/DBPSWD/$DBPSWD/g" \
-    -e "s/DBPREFIX/$DBPREFIX/g" \
-    -e "s/DBHOST/$DBHOST/g" \
-    -e "s/DBPORT/$DBPORT/g" \
-    -e "s/DRUPALHASH/$DRUPALHASH/g" \
-    -e "s/DRUPALTRUSTEDHOST/$DRUPALTRUSTEDHOST/g"
+  # that was sudo
+  sed -i /var/www/drupal/root/drupal_settings.php \
+    -e "s/DBNAME/${DBNAME:-}/g" \
+    -e "s/DBUSER/${DBUSER:-}/g" \
+    -e "s/DBPSWD/${DBPSWD:-}/g" \
+    -e "s/DBPREFIX/${DBPREFIX:-}/g" \
+    -e "s/DBHOST/${DBHOST:-}/g" \
+    -e "s/DBPORT/${DBPORT:-}/g" \
+    -e "s/DRUPALHASH/${DRUPALHASH:-}/g"
+  #  -e "s|DRUPALTRUSTEDHOST|$DRUPALTRUSTEDHOST/g"
   # sudo cat /var/www/drupal/root/drupal_settings.php >> /var/www/drupal/html/web/sites/default/settings.php
-  sudo cat /var/www/drupal/root/drupal_settings.php >> /var/www/drupal/settings/settings.php
-  sudo chown 1000:1000 /var/www/drupal/settings/settings.php
+  # that was sudo
+  sed -i /var/www/drupal/root/drupal_settings.php \
+    -e "s|DRUPALTRUSTEDHOST|${DRUPALTRUSTEDHOST:-}|g"
+  # that was sudo
+  cat /var/www/drupal/root/drupal_settings.php >> /var/www/drupal/settings/settings.php
+  # sudo chown 1000:1000 /var/www/drupal/settings/settings.php
   # chown 1000:1000 /var/www/drupal/html/web/sites/default/settings.php
   # mv /var/www/drupal/html/web/sites/default/settings.php /var/www/drupal/settings/settings.php
 fi
@@ -28,10 +36,11 @@ fi
 # chown -R www-data:www-data /var/www/drupal/
 # settings is special, it should be set to ready-only for all
 # sudo chmod 444 /var/www/drupal/html/web/sites/default/settings.php
-sudo chmod 444 /var/www/drupal/settings/settings.php
+# all the next three was sudo
+chmod 444 /var/www/drupal/settings/settings.php
 # to develop custom modules/themes they need group set to rw
-sudo chmod -R 775 /var/www/drupal/html/web/modules/custom/
-sudo chmod -R 775 /var/www/drupal/html/web/themes/custom/
+chmod -R 775 /var/www/drupal/html/web/modules/custom/
+chmod -R 775 /var/www/drupal/html/web/themes/custom/
 
 if [[ -f /var/www/drupal/html/web/sites/default/settings.php ]]
 then
@@ -42,6 +51,7 @@ else
 fi
 
 # chown -R www-data:www-data /var/www/drupal/html/web/sites/default/
+# sudo chown -R 1000:1000 /var/www/drupal/
 
 sudo apache2ctl -D FOREGROUND
 
